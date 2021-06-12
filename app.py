ec6919d9de6b00a354c13cddf135170e713f04ca
Hanke Chen <i@kokecacao.me>
2021-06-11 23:40:54 -0400

    :tada: add launch description README

--- app.py
+++ app.py
@@ -2,3 +2,31 @@ from project import app
 
 if __name__ == "__main__":
     app.run()
+
+# TO RUN THIS PROJECT
+# > conda activate web
+# > python app.py
+
+# TO RUN ANDROID APP
+# sudo javaconfig
+# (and choose 0, java 11)
+# android-studio
+# (use command prompt (zsh) to open android studio)
+
+# TO CREATE DATABASE
+# send this to http://127.0.0.1:5000/graphql
+# mutation {
+#     test(key: "Koke_Cacao 's secret key", action: "add to db") {
+#         success
+#     }
+# }
+# and you should recieve
+# {
+#     "data": {
+#         "test": {
+#             "success": true
+#         }
+#     },
+#     "extensions": {},
+#     "errors": null
+# }
